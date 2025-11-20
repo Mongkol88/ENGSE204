e215bfebf0b3dedd72fac87d051ed628800c624a
unknown <flucktv25@gmail.com>
2025-11-20 17:26:22 +0700

    Fix lab1.2

--- Lab1/Lab1P2/lab1P2.java
+++ Lab1/Lab1P2/lab1P2.java
@@ -7,13 +7,19 @@ public class lab1P2 {
 
         Scanner input = new Scanner(System.in);
 
+        System.out.println( "Enter Width : " ) ; 
+
         double W = input.nextDouble() ;
+
+        System.out.println( "Enter Height : " ) ; 
 
         double H = input.nextDouble() ; 
 
         double ResultArea = H * W ;
 
-        System.out.println(ResultArea) ; 
+        System.out.println( "Result :" ) ; 
+
+        System.out.println( ResultArea ) ;
 
         input.close();
     }
