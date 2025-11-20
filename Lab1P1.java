2988618f4afd574b21ace9e502a120e27d137926
unknown <flucktv25@gmail.com>
2025-11-20 16:24:00 +0700

    Test Commit 001

---
+++ Lab1P1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Lab1P1 {public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    int x = input.nextInt() ;
+
+    int y = input.nextInt() ;
+    
+    System.out.println( x + y );
+
+    input.close();
+    }
+}
