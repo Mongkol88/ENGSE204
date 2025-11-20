73d17923e64e8bc5b9ed24e4b1e358b0929e1a8e
unknown <flucktv25@gmail.com>
2025-11-20 16:42:45 +0700

    add Lab1.1

--- Lab1/Lab1P1/Lab1P1.java
+++ Lab1/Lab1P1/Lab1P1.java
@@ -1,13 +1,18 @@
-import java.util.Scanner;
+package Lab1.Lab1P1 ; 
+
+import java.util.Scanner ;
 
 public class Lab1P1 {public static void main(String[] args) {
+    
     Scanner input = new Scanner(System.in);
 
     int x = input.nextInt() ;
 
     int y = input.nextInt() ;
+
+    int result = x + y ;
     
-    System.out.println( x + y );
+    System.out.println( result );
 
     input.close();
     }
